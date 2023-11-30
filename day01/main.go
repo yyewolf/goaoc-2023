e1d2618df485567f1a1cbb127d4bedbca5a2142c
Yewolf <yyewolf@gmail.com>
2023-11-30 23:04:16 +0100

    feat: day01 preparations

---
+++ day01/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Part one
+	// elves, err := parse("input.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// answer := doPartOne(elves)
+	// fmt.Println(answer)
+
+	// answer = doPartTwo(elves)
+	// fmt.Println(answer)
+}
+
+// func doPartOne() int {
+// 	return 0
+// }
---
+++ day01/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+var result int
+
+// func BenchmarkPartOne(b *testing.B) {
+// 	elves, err := parse("input.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	var r int
+// 	for n := 0; n < b.N; n++ {
+// 		r = doPartOne(elves)
+// 	}
+// 	result = r
+// }
+
+// func BenchmarkPartTwo(b *testing.B) {
+// 	elves, err := parse("input.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	var r int
+// 	for n := 0; n < b.N; n++ {
+// 		r = doPartTwo(elves)
+// 	}
+// 	result = r
+// }
+
+// func BenchmarkPartOneParsing(b *testing.B) {
+// 	var r int
+// 	for n := 0; n < b.N; n++ {
+// 		elves, err := parse("input.txt")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		r = doPartOne(elves)
+// 	}
+// 	result = r
+// }
+
+// func BenchmarkPartTwoParsing(b *testing.B) {
+// 	var r int
+// 	for n := 0; n < b.N; n++ {
+// 		elves, err := parse("input.txt")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		r = doPartTwo(elves)
+// 	}
+// 	result = r
+// }
