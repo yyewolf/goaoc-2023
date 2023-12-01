513d15520c38b25e94f21a8c8cd37eec55af2af6
Yewolf <yyewolf@gmail.com>
2023-12-01 22:32:22 +0100

    feat: opti

--- day01/main.go
+++ day01/main.go
@@ -2,65 +2,135 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
+	"runtime"
 )
 
-func main() {
-	// Part one
-	lines, err := parse("input.txt")
-	if err != nil {
-		panic(err)
-	}
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var _ [100000000]byte
+}
 
-	answer := doPartOne(lines)
+func main() {
+	answer := doPartOne(input)
 	fmt.Println(answer)
 
-	answer = doPartTwo(lines)
+	answer = doPartTwo(input)
 	fmt.Println(answer)
 }
 
-func doPartOne(file string) int {
-	r := regexp.MustCompile(`[^\d\n]+`)
-	file = r.ReplaceAllString(file, "")
-	lines := strings.Split(file, "\n")
+func doPartOne(file []byte) int {
 	sum := 0
-	for _, line := range lines {
-		first := line[0]
-		last := line[len(line)-1]
-		I, _ := strconv.Atoi(fmt.Sprintf("%c%c", first, last))
-		sum += I
+
+	first := -1
+	last := -1
+	for _, c := range file {
+		I := int(c - '0')
+		if I < 0 || I > 9 {
+			if c == '\n' {
+				sum += first<<3 + first<<1 + last
+				first = -1
+				last = -1
+			}
+		} else {
+			if first == -1 {
+				first = I
+				last = I
+			} else {
+				last = I
+			}
+		}
 	}
+
+	sum += first<<3 + first<<1 + last
+
 	return sum
 }
 
-var NumToWord = map[int]string{
-	0: "zero",
-	1: "one",
-	2: "two",
-	3: "three",
-	4: "four",
-	5: "five",
-	6: "six",
-	7: "seven",
-	8: "eight",
-	9: "nine",
+func doFirstLast(first, last *int, I int) {
+	if *first == -1 {
+		*first = I
+		*last = I
+	} else {
+		*last = I
+	}
 }
 
-func doPartTwo(file string) int {
-	// First we replace spelled numbers with digits
-	for k, v := range NumToWord {
-		file = strings.ReplaceAll(file, v, fmt.Sprintf("%s%d%s", v, k, v))
+func doPartTwo(file []byte) int {
+	sum := 0
+
+	first := -1
+	last := -1
+	var rotatingBuffer = make([]byte, 6)
+	for i, c := range file {
+		if i >= 6 {
+			rotatingBuffer = file[i-6 : i]
+		} else {
+			rotatingBuffer = append(rotatingBuffer, c)
+		}
+
+		switch rotatingBuffer[len(rotatingBuffer)-1] {
+		case 'o':
+			switch rotatingBuffer[len(rotatingBuffer)-2] {
+			case 'w':
+				if rotatingBuffer[len(rotatingBuffer)-3] == 't' {
+					doFirstLast(&first, &last, 2)
+				}
+			case 'r':
+				if rotatingBuffer[len(rotatingBuffer)-3] == 'e' && rotatingBuffer[len(rotatingBuffer)-4] == 'z' {
+					doFirstLast(&first, &last, 0)
+				}
+			}
+		case 'e':
+			switch rotatingBuffer[len(rotatingBuffer)-2] {
+			case 'n':
+				switch rotatingBuffer[len(rotatingBuffer)-3] {
+				case 'i':
+					if rotatingBuffer[len(rotatingBuffer)-4] == 'n' {
+						doFirstLast(&first, &last, 9)
+					}
+				case 'o':
+					doFirstLast(&first, &last, 1)
+				}
+			case 'v':
+				if rotatingBuffer[len(rotatingBuffer)-3] == 'i' && rotatingBuffer[len(rotatingBuffer)-4] == 'f' {
+					doFirstLast(&first, &last, 5)
+				}
+			case 'e':
+				if rotatingBuffer[len(rotatingBuffer)-3] == 'r' && rotatingBuffer[len(rotatingBuffer)-4] == 'h' && rotatingBuffer[len(rotatingBuffer)-5] == 't' {
+					doFirstLast(&first, &last, 3)
+				}
+			}
+		case 'n':
+			if rotatingBuffer[len(rotatingBuffer)-2] == 'e' && rotatingBuffer[len(rotatingBuffer)-3] == 'v' && rotatingBuffer[len(rotatingBuffer)-4] == 'e' && rotatingBuffer[len(rotatingBuffer)-5] == 's' {
+				doFirstLast(&first, &last, 7)
+			}
+		case 't':
+			if rotatingBuffer[len(rotatingBuffer)-2] == 'h' && rotatingBuffer[len(rotatingBuffer)-3] == 'g' && rotatingBuffer[len(rotatingBuffer)-4] == 'i' && rotatingBuffer[len(rotatingBuffer)-5] == 'e' {
+				doFirstLast(&first, &last, 8)
+			}
+		case 'x':
+			if rotatingBuffer[len(rotatingBuffer)-2] == 'i' && rotatingBuffer[len(rotatingBuffer)-3] == 's' {
+				doFirstLast(&first, &last, 6)
+			}
+		case 'r':
+			if rotatingBuffer[len(rotatingBuffer)-2] == 'u' && rotatingBuffer[len(rotatingBuffer)-3] == 'o' && rotatingBuffer[len(rotatingBuffer)-4] == 'f' {
+				doFirstLast(&first, &last, 4)
+			}
+		}
+
+		I := int(c - '0')
+		if I < 0 || I > 9 {
+			if c == '\n' {
+				sum += first<<3 + first<<1 + last
+				first = -1
+				last = -1
+			}
+		} else {
+			doFirstLast(&first, &last, I)
+		}
 	}
 
-	r := regexp.MustCompile(`[^\d\n]+`)
-	file = r.ReplaceAllString(file, "")
-	lines := strings.Split(file, "\n")
-	sum := 0
-	for _, line := range lines {
-		I, _ := strconv.Atoi(fmt.Sprintf("%c%c", line[0], line[len(line)-1]))
-		sum += I
-	}
+	sum += first<<3 + first<<1 + last
+
 	return sum
 }
--- day01/main_test.go
+++ day01/main_test.go
@@ -5,49 +5,17 @@ import "testing"
 var result int
 
 func BenchmarkPartOne(b *testing.B) {
-	file, err := parse("input.txt")
-	if err != nil {
-		panic(err)
-	}
 	var r int
 	for n := 0; n < b.N; n++ {
-		r = doPartOne(file)
+		r = doPartOne(input)
 	}
 	result = r
 }
 
 func BenchmarkPartTwo(b *testing.B) {
-	file, err := parse("input.txt")
-	if err != nil {
-		panic(err)
-	}
 	var r int
 	for n := 0; n < b.N; n++ {
-		r = doPartTwo(file)
-	}
-	result = r
-}
-
-func BenchmarkPartOneParsing(b *testing.B) {
-	var r int
-	for n := 0; n < b.N; n++ {
-		file, err := parse("input.txt")
-		if err != nil {
-			panic(err)
-		}
-		r = doPartOne(file)
-	}
-	result = r
-}
-
-func BenchmarkPartTwoParsing(b *testing.B) {
-	var r int
-	for n := 0; n < b.N; n++ {
-		file, err := parse("input.txt")
-		if err != nil {
-			panic(err)
-		}
-		r = doPartTwo(file)
+		r = doPartTwo(input)
 	}
 	result = r
 }
